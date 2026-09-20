79ca5ed4f0af9d9a7d2135bfd3556f1af3fa8e16
aista12 <sabrinaxurramova2@gmail.com>
2026-09-21 03:22:12 +0500

    feat: leaderboard layout

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,12 @@
 import '../shared/styles/globals.scss';
 import { createCarousel } from '../pages/home/carousel';
 import { createHero } from '../pages/home/hero';
+import { createLeaderboard } from '../pages/home/leaderboard';
 import { createHeader } from '../widgets/header/header';
 
 const app = document.createElement('div');
 
 app.id = 'app';
-app.append(createHeader(), createHero(), createCarousel());
+app.append(createHeader(), createHero(), createCarousel(), createLeaderboard());
 
 document.body.append(app);
