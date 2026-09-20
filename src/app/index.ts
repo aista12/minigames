569cf714f858d49dc29b58ba8d0d5c0046d15b39
aista12 <sabrinaxurramova2@gmail.com>
2026-09-21 03:07:47 +0500

    feat: hero section

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,10 @@
 import '../shared/styles/globals.scss';
+import { createHero } from '../pages/home/hero';
 import { createHeader } from '../widgets/header/header';
 
 const app = document.createElement('div');
 
 app.id = 'app';
-app.append(createHeader());
+app.append(createHeader(), createHero());
 
 document.body.append(app);
