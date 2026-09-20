1548cdd3f9fd7fd33f0d4756fb3cd0ec644b1e3b
aista12 <sabrinaxurramova2@gmail.com>
2026-09-21 04:36:11 +0500

    chore: organized folders

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,9 @@
 import '../shared/styles/globals.scss';
-import { createCarousel } from '../pages/home/carousel';
-import { createDeveloperCta } from '../pages/home/developer-cta';
-import { createFooter } from '../pages/home/footer';
-import { createHero } from '../pages/home/hero';
-import { createLeaderboard } from '../pages/home/leaderboard';
+import { createCarousel } from '../pages/home/carousel/carousel';
+import { createDeveloperCta } from '../pages/home/developer-cta/developer-cta';
+import { createFooter } from '../pages/home/footer/footer';
+import { createHero } from '../pages/home/hero/hero';
+import { createLeaderboard } from '../pages/home/leaderboard/leaderboard';
 import { createHeader } from '../widgets/header/header';
 
 const app = document.createElement('div');
