8b19b1ec2ea39302ce7c9292f547fc704fe89eb6
aista12 <sabrinaxurramova2@gmail.com>
2026-09-21 03:26:56 +0500

    feat: gamedev layout

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import '../shared/styles/globals.scss';
 import { createCarousel } from '../pages/home/carousel';
+import { createDeveloperCta } from '../pages/home/developer-cta';
 import { createHero } from '../pages/home/hero';
 import { createLeaderboard } from '../pages/home/leaderboard';
 import { createHeader } from '../widgets/header/header';
@@ -7,6 +8,12 @@ import { createHeader } from '../widgets/header/header';
 const app = document.createElement('div');
 
 app.id = 'app';
-app.append(createHeader(), createHero(), createCarousel(), createLeaderboard());
+app.append(
+    createHeader(),
+    createHero(),
+    createCarousel(),
+    createLeaderboard(),
+    createDeveloperCta(),
+);
 
 document.body.append(app);
