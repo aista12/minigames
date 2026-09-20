de32e4b58b3cb3f338db195574ae4de13371ca5b
aista12 <sabrinaxurramova2@gmail.com>
2026-09-21 03:16:29 +0500

    feat: carousel layout

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,11 @@
 import '../shared/styles/globals.scss';
+import { createCarousel } from '../pages/home/carousel';
 import { createHero } from '../pages/home/hero';
 import { createHeader } from '../widgets/header/header';
 
 const app = document.createElement('div');
 
 app.id = 'app';
-app.append(createHeader(), createHero());
+app.append(createHeader(), createHero(), createCarousel());
 
 document.body.append(app);
